76185ac7e979c1ce82dbf7a69ddeee10cf229af1
jony420 <65048681+jony420@users.noreply.github.com>
2025-05-27 21:39:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template, request
-from stock_analysis import analyze_stock
+from stock_analysis import analyze_stock, get_recommended_stocks
+import os
 
 #work is work
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    print("🔍 Route was reached")
     signal = None
-    symbol = "NVDA"
+    symbol = ""
     if request.method == "POST":
         symbol = request.form["symbol"].upper()
         print(f"Analyzing: {symbol}")  # ADD THIS
         signal = analyze_stock(symbol)
+
     return render_template("index.html", signal=signal, symbol=symbol)
 
+@app.route("/recommend")
+def recommended():
+    recommended = get_recommended_stocks()
+    return render_template("recommended.html", recommended=recommended)
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
