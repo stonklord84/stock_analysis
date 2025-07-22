0aa96059f0df6a534395da5531a5aea76740881d
stonklord84 <65048681+stonklord84@users.noreply.github.com>
2025-07-21 20:10:44 -0400

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -9,7 +9,52 @@ import requests
 
 popular_symbols = ["AAPL","MSFT","NVDA","GOOGL","AMZN","TSLA","META","BRK.B","JPM","V"]
 
-def analyze_stock(symbol):
+def is_valid_stock(symbol):
+    stock = yf.Ticker(symbol)
+    try:
+        info = stock.info
+        return "longName" in info and info['regularMarketPrice'] is not None
+    except Exception:
+        return False
+
+def check_stock(symbol):
+    conn = sqlite3.connect("data/stock_data.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name=?", (symbol,)
+    )
+    
+    if cursor.fetchone():
+        cursor.execute(
+            f"select MAX(Date) FROM '{symbol}'"
+        )
+        latest_date = cursor.fetchone()[0]
+        if latest_date:
+            latest_date = pd.to_datetime(latest_date)
+            today = pd.Timestamp.today().normalize()
+
+            if latest_date >= today - pd.Timedelta(days=1):
+                data = pd.read_sql(f"SELECT * FROM '{symbol}'", conn, parse_dates=["Date"])
+                
+                generate_chart(symbol, data)
+                conn.close()
+                return generate_stock_signal(symbol, data)
+    data = yf.download(symbol, period="6mo", interval="1d")
+    if data.empty or len(data) < 20:
+        return None
+    
+    if isinstance(data.columns, pd.MultiIndex):
+        data.columns = data.columns.get_level_values(0)
+
+    data.reset_index(inplace=True)  # Make sure "Date" is a column, not an index
+    data.to_sql(symbol, conn, if_exists="replace", index=False)
+    conn.close()
+
+    generate_chart(symbol, data)
+    return generate_stock_signal(symbol, data)
+
+
+#def analyze_stock(symbol):
     # Open the database
     conn = sqlite3.connect("data/stock_data.db")
     cursor = conn.cursor()
@@ -54,6 +99,7 @@ def analyze_stock(symbol):
         print(f"Skipping {symbol} — no data or too little data.")
         conn.close()
         return None
+    
 
     # Save data to DB
     # After downloading from yfinance
@@ -68,6 +114,49 @@ def analyze_stock(symbol):
 
     data.set_index("Date", inplace=True)
     return generate_chart_and_signal(symbol, data)
+
+def generate_chart(symbol, data):
+    data["MA5"] = data["Close"].rolling(window=5).mean()
+    data["MA20"] = data["Close"].rolling(window=20).mean()
+    data["Signal"] = 0
+
+    data.loc[data["MA5"] > data["MA20"], "Signal"] = 2
+    data.loc[data["MA5"] < data["MA20"], "Signal"] = -2
+
+    # Save chart
+    data[["Close", "MA5", "MA20"]].plot(figsize=(10, 5), title=f"{symbol} Price & MAs")
+    plt.grid()
+    plt.savefig("static/output.png")
+    plt.close()
+
+def generate_stock_signal(symbol, data):
+    if "Signal" not in data.columns or data.empty:
+        return None
+    signal = 0
+    signal += avg_crossover(data)
+    fair_value = generate_fair_value(symbol)
+    if fair_value[0] < fair_value[1]:
+        signal -= 1
+    else:
+        signal += 1
+    vol = volatility(data)
+    if vol < 0.01:
+        signal += 1
+    elif 0.01 <= vol <= 0.2:
+        signal += 0
+    else:
+        signal -= 1
+    if signal == 1:
+        return "BULLISH- BUY"
+    elif signal == 0:
+        return "HOLD"
+    else:
+        return "SELL"
+    
+
+def avg_crossover(data):
+    signal = data["Signal"].iloc[-1]
+    return signal
 
 
 def generate_fair_value(symbol):
@@ -102,55 +191,22 @@ def generate_fair_value(symbol):
     except Exception as e:
         print(f"Error calculating fair value for {symbol}: {e}")
         return None
-
-
-        
-
-"""def precompute_all():
-    for symbol in popular_symbols:
-        try:
-            print(f"Precomputing {symbol}...")
-            analyze_stock(symbol)
-        except Exception as e:
-            print(f"Failed on {symbol}: {e}")"""
+    
+def volatility(data):
+    data["Return"] = data["Close"].pct_change()
+    volatility = data["Return"].std()
+    return volatility
 
 
 def get_recommended_stocks() -> list:
     recommended = []
 
     for symbol in popular_symbols:
-        signal = analyze_stock(symbol)
-        if signal == "BULLISH - BUY":
+        signal = check_stock(symbol)
+        if signal > 0:
             recommended.append(symbol)
 
     return recommended
-
-def generate_chart_and_signal(symbol, data):
-    data["MA5"] = data["Close"].rolling(window=5).mean()
-    data["MA20"] = data["Close"].rolling(window=20).mean()
-    data["Signal"] = 0
-    data.loc[data["MA5"] > data["MA20"], "Signal"] = 1
-    data.loc[data["MA5"] < data["MA20"], "Signal"] = -1
-
-    latest_signal = data["Signal"].iloc[-1]
-
-    # Save chart
-    data[["Close", "MA5", "MA20"]].plot(figsize=(10, 5), title=f"{symbol} Price & MAs")
-    plt.grid()
-    plt.text(
-        data.index[-1],
-        data["Close"].iloc[-1] * 1.01,
-        "BULLISH - BUY" if latest_signal == 1 else "SELL" if latest_signal == -1 else "HOLD",
-        color="green" if latest_signal == 1 else "red" if latest_signal == -1 else "gray",
-        fontsize=12,
-        ha="right"
-    )
-    plt.savefig("static/output.png")
-    plt.close()
-
-    signal_label = "BULLISH - BUY" if latest_signal == 1 else "SELL" if latest_signal == -1 else "HOLD"
-    return signal_label
-
 
 
 from datetime import datetime, timedelta
